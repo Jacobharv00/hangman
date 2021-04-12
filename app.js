f10eb389ad00d48e4d03fc7cf7e7dae630a3e4d3
Jacobharv00 <jacobharv00@gmail.com>
2021-04-12 13:50:28 -0600

    Inital commit

---
+++ app.js
@@ -0,0 +1,114 @@
+// Getting things to show up in the browser by <p> id from index.html file
+const puzzleEl = document.querySelector('#puzzle')
+const guessesEl = document.querySelector('#guesses')
+const game1 = new Hangman('Cat', 2)
+
+puzzleEl.textContent = game1.puzzle
+guessesEl.textContent = game1.statusMessage
+
+
+window.addEventListener('keypress', (e) => {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
+})
+
+ getPuzzle('2').then((puzzle) => {
+  console.log(puzzle)
+ }).catch((err) => {
+    console.log(`Error: ${err}`)
+ })
+
+getLocation().then((location) => {
+  return getCountry(location.country)
+}).then((country) => {
+    console.log(country.name)
+}).catch((err) => {
+    console.log(`Error: ${err}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// Practice with fetch()
+// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
+//     if (response.status === 200) {
+//        return response.json()
+//     } else {
+//         throw new Error('Unable to fetch the puzzle')
+//     }
+// }).then((data) => {
+//     console.log(data.puzzle)
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Making an HTTP request
+// const request = new XMLHttpRequest()
+
+// request.addEventListener('readystatechange', (e) => {
+//   if (e.target.readyState === 4 && e.target.status === 200) {
+//     const data = JSON.parse(e.target.responseText)
+//     console.log(data)
+//   } else if (e.target.readyState === 4) {
+//     console.log('An error has taken place')
+//   }
+// })
+
+// request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+// request.send()
+
+
+// // Challange area for making my own http request
+// const countryCode = "US"
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if(e.target.readyState === 4 && e.target.status === 200) {
+//       const data = JSON.parse(e.target.responseText)
+//       const country = data.find((country) => country.alpha2Code === countryCode)
+//       console.log(country.name) // <-- Prints United States Of America
+//   } else if (e.target.readyState === 4) {
+//     console.log('Unable to fetch data')
+//   } 
+// })
+
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
+
+
+// Primitive value: string, number, boolean, null, undefinded
+
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+// HTTP (Hypertext Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done
+// httpstatuses.com for http status codes
+// mdnhttpmessage.com for http messages for requests and responses
