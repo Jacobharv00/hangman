1bdeaa7aee29e67d24943976a87f73899e99a08c
Jacob Harvey <Jacobharv00@gmail.com>
2021-04-13 18:23:39 -0600

    Learned about async-await, changed some code.

--- app.js
+++ app.js
@@ -1,32 +1,68 @@
 // Getting things to show up in the browser by <p> id from index.html file
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Cat', 2)
-
-puzzleEl.textContent = game1.puzzle
-guessesEl.textContent = game1.statusMessage
-
+let game1 
 
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
+    render()
+})
+
+const render = () => {
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
-})
+}
 
- getPuzzle('2').then((puzzle) => {
-  console.log(puzzle)
- }).catch((err) => {
-    console.log(`Error: ${err}`)
- })
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
 
-getLocation().then((location) => {
-  return getCountry(location.country)
-}).then((country) => {
-    console.log(country.name)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-})
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  getPuzzle('2').then((puzzle) => {
+//   console.log(puzzle)
+//  }).catch((err) => {
+//     console.log(`Error: ${err}`)
+//  })
+
+
+
+//  getCurrentCountry().then((country) => {
+//     console.log(country.name)
+//  }).catch((error) => {
+//     console.log(error)
+//  })
+
+
+
+
+
+ // Old way before async-await
+// getLocation().then((location) => {
+//   return getCountry(location.country)
+// }).then((country) => {
+//     console.log(country.name)
+// }).catch((err) => {
+//     console.log(`Error: ${err}`)
+// })
 
 
 
--- requests.js
+++ requests.js
@@ -1,36 +1,89 @@
-const getPuzzle = (wordCount) => {
-  return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-      if (response.status === 200) {
-         return response.json()
-      } else {
-      throw new Error('Unable to fetch puzzle')
-      }
-   }).then((data) => {
+const getPuzzle = async (wordCount) => {
+   const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+   
+   if (response.status === 200) {
+      const data = await response.json()
       return data.puzzle
-   })
+   } else {
+      throw new Error('Unable to get puzzle')
+   }
 }
 
-const getCountry = (countryCode) => {
-   return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
-      if (response.status === 200) {
-         return response.json()
-      } else {
-         throw new Error('Unable to fetch data')
-      }
-   }).then((data) => data.find((country) => country.alpha2Code === countryCode))
+const getCurrentCountry = async () => {
+   const location = await getLocation()
+   const country = await getCountry(location.country)
+   return country
 }
 
-const getLocation = () => {
-   return fetch('https://ipinfo.io/json?token=1131d42dba1c77').then((response) => {
-      if (response.status === 200) {
-         return response.json()
-      } else {
-         throw new Error('Unable to fetch location')
-      }
-   })
+const getCountry = async (countryCode) => {
+   const response = await fetch('http://restcountries.eu/rest/v2/all')
+
+   if  (response.status === 200) {
+      const data = await response.json()
+      return data.find((country) => country.alpha2Code === countryCode)
+   } else {
+      throw new Error('Unable to fetch data')
+   }
 }
+  
+const getLocation = async () => {
+   const response = await fetch('https://ipinfo.io/json?token=1131d42dba1c77')
+
+   if (response.status === 200) {
+      return response.json()
+   } else {
+      throw new Error('Unable to fetch location')
+   }
+}
+   
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// Old way not using async-await
+// const getPuzzleOld = (wordCount) => {
+//    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+//        if (response.status === 200) {
+//           return response.json()
+//        } else {
+//        throw new Error('Unable to fetch puzzle')
+//        }
+//     }).then((data) => {
+//        return data.puzzle
+//     })
+//  }
+
+// old way before without using async-await
+// const getCountry = (countryCode) => {
+//    return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
+//       if (response.status === 200) {
+//          return response.json()
+//       } else {
+//          throw new Error('Unable to fetch data')
+//       }
+//    }).then((data) => data.find((country) => country.alpha2Code === countryCode))
+// }
+
+// old way before without using async-await
+// const getLocation = () => {
+//    return fetch('https://ipinfo.io/json?token=1131d42dba1c77').then((response) => {
+//       if (response.status === 200) {
+//          return response.json()
+//       } else {
+//          throw new Error('Unable to fetch location')
+//       }
+//    })
+// }
 
 
 // Old way using new XMLHttpRequest()
