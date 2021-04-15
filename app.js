8ffe9c20dcd1365200c7ddcd7a17739be09b057b
Jacob Harvey <Jacobharv00@gmail.com>
2021-04-14 19:35:05 -0600

    Styled Hangman game! Leaned css basics

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ window.addEventListener('keypress', (e) => {
 })
 
 const render = () => {
-    puzzleEl.textContent = game1.puzzle
+    puzzleEl.innerHTML = ''
     guessesEl.textContent = game1.statusMessage
+
+    game1.puzzle.split('').forEach((letter) => {
+        const letterEl = document.createElement('span')
+        letterEl.textContent = letter
+        puzzleEl.appendChild(letterEl)
+    })
 }
 
 const startGame = async () => {
